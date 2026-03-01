5156f654053a3ed40e1fbe1eb066f494991b47b1
Agustín Segade <laaraniaqpica@gmail.com>
2026-03-01 19:52:18 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10670975
+    "zoneId": 10671012
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
